fe9e2c047cd6363c454242a5ec4e2c84b937a3c6
Shuhrat <shukhrat.shokirov@alif.uz>
2025-02-10 08:35:59 +0500

    add worker pool

--- internal/gophermart/core/application/worker.go
+++ internal/gophermart/core/application/worker.go
@@ -3,8 +3,10 @@ package application
 import (
 	"context"
 	"errors"
+	"sync"
 
 	"gofermart/internal/gophermart/core/client"
+	"gofermart/internal/gophermart/core/model"
 )
 
 func (a *Application) handleOrders(ctx context.Context) {
@@ -18,28 +20,66 @@ func (a *Application) handleOrders(ctx context.Context) {
 		return
 	}
 
+	jobs := make(chan model.Order, len(orders))
+	results := make(chan error, len(orders))
+
+	var (
+		wg         sync.WaitGroup
+		maxWorkers = 5
+	)
+
+	for i := 0; i < maxWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for order := range jobs {
+				if err := a.processOrder(ctx, order); err != nil {
+					results <- err
+					if errors.Is(err, client.ErrTooManyRequests) {
+						break
+					}
+				}
+			}
+		}()
+	}
+
 	for _, order := range orders {
-		resp, err := a.client.SendOrder(ctx, order.OrderID)
+		jobs <- order
+	}
+	close(jobs)
+
+	wg.Wait()
+	close(results)
+
+	for err := range results {
 		if err != nil {
-			if errors.Is(err, client.ErrTooManyRequests) {
-				return
-			}
-
-			if errors.Is(err, client.ErrNotFound) {
-				continue
-			}
-
-			a.logger.Errorf("can't send order %s: %v", order.OrderID, err)
-			continue
-		}
-
-		var amount int
-		if resp.Accrual != nil {
-			amount = convertToPence(*resp.Accrual)
-		}
-
-		if err := a.repo.SetBalance(ctx, order.OrderID, resp.Status, amount); err != nil {
-			a.logger.Errorf("can't save balance %s: %v", order.OrderID, err)
+			a.logger.Errorf("can't process order: %v", err)
 		}
 	}
+}
+
+func (a *Application) processOrder(ctx context.Context, order model.Order) error {
+	resp, err := a.client.SendOrder(ctx, order.OrderID)
+	if err != nil {
+		if errors.Is(err, client.ErrTooManyRequests) {
+			return err
+		}
+		if errors.Is(err, ErrNotFound) {
+			return nil
+		}
+		a.logger.Errorf("can't send order %s: %v", order.OrderID, err)
+		return err
+	}
+
+	var amount int
+	if resp.Accrual != nil {
+		amount = convertToPence(*resp.Accrual)
+	}
+
+	if err := a.repo.SetBalance(ctx, order.OrderID, resp.Status, amount); err != nil {
+		a.logger.Errorf("can't save balance %s: %v", order.OrderID, err)
+		return err
+	}
+
+	return nil
 }
